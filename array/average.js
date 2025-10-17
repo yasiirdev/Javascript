1ddb57f75219146136b2abb5abba07416d4225fd
Devuno-official <185803128+Yasircodingbt@users.noreply.github.com>
2025-10-17 18:26:44 +0500

    update

--- array/average.js
+++ array/average.js
@@ -1,12 +1,12 @@
 /*
  for a given array with marks of student -> [85,97,44,37,76,60].
-       ## find the average marks of the entire class.
+       ## find the average marks of the entire className.
 */
 
-let arr = [85,97,44,37,76,60];
+let arr = [85, 97, 44, 37, 76, 60];
 let sum = 0;
- for(let val of arr){
-      sum += val ;
- }
-     console.log(sum);
-     console.log(arr.length);
+for (let val of arr) {
+  sum += val;
+}
+console.log(sum);
+console.log(arr.length);
