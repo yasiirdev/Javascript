f9f3c484985b7de09d105c2d1ab1a16a5ddbbcbe
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2025-12-28 18:45:09 +0500

    dropdown in venilla js & css

--- Dom/dropdown_menu/app.js
+++ Dom/dropdown_menu/app.js
@@ -1,18 +1,16 @@
-
-
-function showMenu(){
-     
-      option = document.getElementById("myoption");
- option.style.display === "none"? option.style.display = "grid" : option.style.display="none";
-
-} 
-
+function showMenu() {
+  option = document.getElementById("myoption");
+  option.style.display === "none"
+    ? (option.style.display = "grid")
+    : (option.style.display = "none");
+}
 
 const menu = document.getElementById("mymenu");
-  
-menu.addEventListener("mouseenter",showMenu);   
-   
-   
-   menu.addEventListener("mouseleave",()=>{
-            option.style.display ="none";
-   });
+
+menu.addEventListener("mouseenter", showMenu);
+
+menu.addEventListener("mouseleave", () => {
+  option.style.display = "none";
+});
+
+console.log("kajfh skadfgjh");
